f150bb5391c8a520588cd4ef30f456461945c2c1
Marcus Hultman <marcus.hultman1@gmail.com>
2023-07-18 08:58:44 +0200

    -icons

--- utils/icons/24/outline.ts
+++ utils/icons/24/outline.ts
@@ -1,1 +1,16 @@
-export * from 'https://esm.sh/@heroicons/react@2.0.18/24/outline?alias=react:preact/compat';
+// export * from 'https://esm.sh/@heroicons/react@2.0.18/24/outline?alias=react:preact/compat';
+
+export const EyeIcon = () => {};
+export const Square3Stack3DIcon = () => {};
+export const ChevronLeftIcon = () => {};
+export const ChevronRightIcon = () => {};
+export const EllipsisVerticalIcon = () => {};
+export const PaperAirplaneIcon = () => {};
+export const XMarkIcon = () => {};
+export const ArrowLeftIcon = () => {};
+export const ChatBubbleLeftIcon = () => {};
+export const UserPlusIcon = () => {};
+export const UserIcon = () => {};
+export const EnvelopeIcon = () => {};
+export const CheckIcon = () => {};
+export const ShareIcon = () => {};
