b88eb17e0e6ec352c9a188aae0367dc1bf74e8bd
Marcus Hultman <marcus.hultman1@gmail.com>
2023-07-20 18:29:23 +0200

    print envs

--- utils/icons/24/solid.ts
+++ utils/icons/24/solid.ts
@@ -1,8 +1,8 @@
-// export * from 'https://esm.sh/@heroicons/react@2.0.18/24/solid?alias=react:preact/compat';
+export * from 'https://esm.sh/@heroicons/react@2.0.18/24/solid?alias=react:preact/compat';
 
-export const PlayIcon = () => {};
-export const StopIcon = () => {};
-export const UserIcon = () => {};
-export const UserCircleIcon = () => {};
-export const PuzzlePieceIcon = () => {};
-export const PencilSquareIcon = () => {};
+// export const PlayIcon = () => {};
+// export const StopIcon = () => {};
+// export const UserIcon = () => {};
+// export const UserCircleIcon = () => {};
+// export const PuzzlePieceIcon = () => {};
+// export const PencilSquareIcon = () => {};
