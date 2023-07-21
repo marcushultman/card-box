8ea2bf239d1719ffa9b8f3cef9f77a3ee33e4396
Marcus Hultman <marcus.hultman1@gmail.com>
2023-07-21 11:11:43 +0200

    fix web storage usage

--- utils/use_timestamp.ts
+++ utils/use_timestamp.ts
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'preact/hooks';
 
 export default function (key: string) {
-  const value = localStorage.getItem(key);
+  const storage = 'localStorage' in window ? window.localStorage : undefined;
+  const value = storage?.getItem(key);
   const [date, setDate] = useState<Date>(value ? new Date(value) : new Date());
 
-  useEffect(() => {
-    localStorage.setItem(key, date.toISOString());
-  }, [date]);
+  useEffect(() => storage?.setItem(key, date.toISOString()), [date]);
 
   return [date, () => setDate(new Date())] as const;
 }
